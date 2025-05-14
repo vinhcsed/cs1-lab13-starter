0b16977d07d8e094884d617385a61cd94c2bff4d
Vincent Tran <33761211+VincentVinhTran@users.noreply.github.com>
2025-05-14 12:46:04 -0700

    Update deck.h

--- deck.h
+++ deck.h
@@ -17,14 +17,17 @@ const int MAX_CARDS = 52;
 
 class Deck {
     private:
+        // Vector to represent a deck of Cards
         vector<Card> cards;
+        // Helper function to compare Cards
+        bool comesBefore(const Card&, const Card&);
+        
 
     public:
         Deck();
         Deck(const Deck&);
         ~Deck();
 
-        void operator=(const Deck&);
         bool isEmpty() const;
         int getSize() const;
         Card peek() const;
@@ -34,6 +37,10 @@ class Deck {
         void print(ostream&) const;
 
         // ~~~ New Lab 12 Methods ~~~
+        /* Deck& operator=(const Deck&)
+            update assignment to return this object */
+        Deck& operator=(const Deck&);
+
         /* void Deck::sort()
             sorts the deck in rank/suit order */
         // Must implement using SELECTION SORT
